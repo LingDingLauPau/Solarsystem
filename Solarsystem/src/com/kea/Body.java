2c56105ce48c757508bca66ddf143c4e58c56fbf
LingDingLauPau <kasperjakob@me.com>
2021-09-08 18:59:25 +0200

    abstract and extends framework created

--- Solarsystem/src/com/kea/Body.java
+++ Solarsystem/src/com/kea/Body.java
@@ -3,34 +3,205 @@ package com.kea;
 abstract class Body {
 
     public abstract void rotateRate();
-
+    public abstract void size();
+    public abstract void mass();
+    public abstract void velocity();
+    public abstract void temperature();
+    public abstract void gravitationalForce();
+    public abstract void planets();
+    public abstract void moons();
+    public abstract void comets();
 
 }
 
 class Star extends Body {
 
+    @Override
     public void rotateRate() {
+
+    }
+
+    @Override
+    public void size(){
+
+    }
+
+    @Override
+    public void mass() {
+
+    }
+
+    @Override
+    public void velocity() {
+
+    }
+
+    @Override
+    public void temperature() {
+
+    }
+
+    @Override
+    public void gravitationalForce() {
+
+    }
+
+    @Override
+    public void planets() {
+
+    }
+
+    @Override
+    public void moons() {
+
+    }
+
+    @Override
+    public void comets() {
 
     }
 }
 
 class Planet extends Body {
 
+    @Override
     public void rotateRate() {
+
+    }
+
+    @Override
+    public void size(){
+
+    }
+
+    @Override
+    public void mass() {
+
+    }
+
+    @Override
+    public void velocity() {
+
+    }
+
+    @Override
+    public void temperature() {
+
+    }
+
+    @Override
+    public void gravitationalForce() {
+
+    }
+
+    @Override
+    public void planets() {
+
+    }
+
+    @Override
+    public void moons() {
+
+    }
+
+    @Override
+    public void comets() {
 
     }
 }
 
 class Moon extends Body {
 
+    @Override
     public void rotateRate(){
+
+    }
+
+    @Override
+    public void size(){
+
+    }
+
+    @Override
+    public void mass() {
+
+    }
+
+    @Override
+    public void velocity() {
+
+    }
+
+    @Override
+    public void temperature() {
+
+    }
+
+    @Override
+    public void gravitationalForce() {
+
+    }
+
+    @Override
+    public void planets() {
+
+    }
+
+    @Override
+    public void moons() {
+
+    }
+
+    @Override
+    public void comets() {
 
     }
 }
 
 class Comet extends Body {
 
+    @Override
     public void rotateRate(){
+
+    }
+
+    @Override
+    public void size(){
+
+    }
+
+    @Override
+    public void mass() {
+
+    }
+
+    @Override
+    public void velocity() {
+
+    }
+
+    @Override
+    public void temperature() {
+
+    }
+
+    @Override
+    public void gravitationalForce() {
+
+    }
+
+    @Override
+    public void planets() {
+
+    }
+
+    @Override
+    public void moons() {
+
+    }
+
+    @Override
+    public void comets() {
 
     }
 }
